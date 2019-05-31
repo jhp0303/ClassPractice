c78b690dd0ae8a0f488b462e9dca96c7896a000a
Surplus <yingouyingou@gmail.com>
2019-05-31 16:46:35 +0900

    animals 클래스 깔끔하게 변경

--- practice1/Program.cs
+++ practice1/Program.cs
@@ -8,8 +8,9 @@ namespace practice1
 {
     class Program
     {
-        void practiceRectangle()
+        static void Main(string[] args)
         {
+
             Console.WriteLine(String.Empty);
             Console.WriteLine("Struct");
             Console.WriteLine("=======================");
@@ -27,34 +28,45 @@ namespace practice1
             rect2.print();
 
             //Console.WriteLine("[L] width{0},, length:{1}, Area{2}", rect2.width, rect2.length, rect2.Area());
-        }
 
 
-        static void Main(string[] args)
-        {
             Console.WriteLine(String.Empty);
-            Console.WriteLine("Animal");
+            Console.WriteLine("Class Animal");
             Console.WriteLine("=======================");
 
-            Animal cow = new Animal("소", "Mewww");
+            List<Animal> animals = new List<Animal>();
+            animals.Add(new Animal("fox", "Raww"));
+            animals.Add(new Animal("dog", "Woof"));
+            animals.Add(new Animal("cat", "Mewww"));
 
-            cow.MakeSound();
+            foreach(var animal in animals)
+            {
+                animal.MakeSound();
+            }
+
+            /*  //위랑 같은내용
+            var cat = new Animal("고양이", "Mewww");
+
+            cat.MakeSound();
 
 
-            Animal pig = new Animal("돼지", "Quiq");
-            
-
-            pig.MakeSound();
+            var dog = new Animal("개", "woof");
 
 
-            Animal fox = new Animal("여우", "How");       // Animal = Class, fox = instance, new Animal = 생성자, ("여우","How") = 파라미터
+            dog.MakeSound();
+
+
+            var fox = new Animal("여우", "How");       // Animal = Class, fox = instance, new Animal = 생성자, ("여우","How") = 파라미터
 
             fox.MakeSound();        // function
+            */
+
 
             Console.WriteLine("numberOfAnimals : {0}", Animal.GetNumOfAnimals());
             Console.WriteLine(String.Empty);
             Console.WriteLine("ShapeMath");
             Console.WriteLine("==========================");
+
             Console.WriteLine("Area of Rectangle : {0}", ShapeMath.GetArea(enShape.Rectangle, 5, 6));
             Console.WriteLine("Area of Rectangle : {0}", ShapeMath.GetArea(enShape.Triangle, 5, 6));
             Console.WriteLine("Area of Rectangle : {0}", ShapeMath.GetArea(enShape.Circle, 5));
